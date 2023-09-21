5d70ca30f81991a71f39fc77794af1d3f5109b8f
DecafSunrise <36832027+DecafSunrise@users.noreply.github.com>
2023-09-20 20:06:20 -0400

    refactor, make package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='SimpleTopicModel',
+
+    version='1.0.0',
+
+    author='decafsunrise',
+
+    author_email='alembichosting@gmail.com',
+
+    description='An NLP Package for generating Topic Models',
+
+    packages=find_packages(),
+
+)
